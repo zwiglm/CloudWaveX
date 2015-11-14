9dfdcd7269985d1daf56f1d420cf3d9395912f60
m.papa <m.papa@83b4b492-8abe-1043-8502-780969c7718f>
2015-11-14 15:10:39 +0000

    New project, but only for testing Windows Phone RTC

---
+++ PhotoUploaderUpload/ActualUploader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace PhotoUploaderUpload
+{
+    public sealed class ActualUploader : IBackgroundTask
+    {
+        public ActualUploader()
+        {
+        }
+
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            Debug.WriteLine("YUUUUUU-Huuuuuuu");
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastText02;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+            XmlNodeList textElements = toastXml.GetElementsByTagName("text");
+            textElements[0].AppendChild(toastXml.CreateTextNode("I am here"));
+            ToastNotificationManager.CreateToastNotifier().Show(new ToastNotification(toastXml));
+        }
+
+    }
+}
